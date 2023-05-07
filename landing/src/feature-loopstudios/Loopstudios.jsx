c02ad01f6ff28bf1668b929c96f12f1499d96938
y nakova <yoana.nakova@yahoo.com>
2023-05-07 18:57:30 +0300

    added main jsx file and Heared component/fully responsive

---
+++ landing/src/feature-loopstudios/Loopstudios.jsx
@@ -0,0 +1,16 @@
+import Header from '../feature-loopstudios/components/Header';
+import Article from '../feature-loopstudios/components/Article';
+import Cards from '../feature-loopstudios/components/Cards';
+import Footer from '../feature-loopstudios/components/Footer';
+
+const Loopstudios = () => {
+  return (
+    <div className="font-sans overflow-hidden">
+      <Header />
+      <Article />
+      <Cards />
+      <Footer />
+    </div>
+  );
+};
+export default Loopstudios;
