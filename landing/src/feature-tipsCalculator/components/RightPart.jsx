22517800154bacba33ed3580a0d5f16c9f6d8616
y nakova <yoana.nakova@yahoo.com>
2023-05-11 21:27:25 +0300

    added tablet and mobile view/ need to be fixed some details in UI and need to add more validations of invalid values

--- landing/src/feature-tipsCalculator/components/RightPart.jsx
+++ landing/src/feature-tipsCalculator/components/RightPart.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ResetBtn from './ResetBtn';
 
 const RightPart = (total) => {
+  const [isEmptyContainer, setIsEmptyContainer] = useState(false);
   let totalByPerson = total.byPerson;
 
   if (
@@ -11,17 +12,30 @@ const RightPart = (total) => {
   ) {
     totalByPerson = 0;
   }
-  const resultNumStyle = `text-hoverbuttonOfCalculatorAndRightSideBackground text-6xl flex bold `;
+  useEffect(() => {
+    if (total.total > 0) {
+      setIsEmptyContainer(true);
+    } else {
+      setIsEmptyContainer(false);
+    }
+  }, [total.total]);
+
+  const resultNumStyle = `text-hoverbuttonOfCalculatorAndRightSideBackground text-3xl lg:text-6xl  sm:text-6xl flex bold `;
+  const backgroundStyleisEmpty = isEmptyContainer
+    ? 'bg-buttonOfCalculatorAndRightSideBackground lg:w-[420px] rounded-lg p-10 lg:h-[417px] mr-[5%] ml-[5%]'
+    : 'bg-white  lg:w-[420px] rounded-lg p-10 lg:h-[417px] mr-[5%] ml-[5%]';
 
   return (
-    <div className="bg-buttonOfCalculatorAndRightSideBackground w-[420px] h-[417px] rounded-lg p-10">
+    <div className={backgroundStyleisEmpty}>
       <div className="text-white flex justify-between pt-5">
         <div>
           Tip Amount
           <p className="text-slate-300">/person</p>
         </div>
         <div className={resultNumStyle}>
-          <div className="text-4xl pt-2 font-black">$</div>
+          <div className="text-3xl lg:text-4xl  sm:text-4xl p-0 lg:pt-2 font-black">
+            $
+          </div>
           {totalByPerson.toFixed(2)}
         </div>
       </div>
@@ -31,7 +45,9 @@ const RightPart = (total) => {
           <p className="text-slate-300">/person</p>
         </div>
         <div className={resultNumStyle}>
-          <div className="text-4xl pt-2 font-black">$</div>
+          <div className="text-3xl lg:text-4xl  sm:text-4xl lg:pt-2 font-black">
+            $
+          </div>
 
           {total.total.toFixed(2)}
         </div>
