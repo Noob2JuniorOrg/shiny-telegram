1486a9efea9a63e6d7c18c1f3ff2124dd44aab69
y nakova <yoana.nakova@yahoo.com>
2023-05-17 16:59:14 +0300

    successful fetch request to gitHub/

---
+++ GitHubFindDev.jsx
@@ -0,0 +1,64 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+export const UserContext = createContext();
+const initialState = {
+  name: null,
+  login: null,
+  timeStamp: null,
+  bio: null,
+  repos: null,
+  followers: null,
+  following: null,
+  location: null,
+};
+
+const GitHubFindDev = () => {
+  const [user, setUser] = useState(initialState);
+
+  const useFetchData = (userName) => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${userName}`,
+          {
+            headers: {
+              Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_KEY}`,
+            },
+          }
+        );
+        const data = await response.json();
+
+        setUser(() => ({
+          name: data.name,
+          login: data.login,
+          timeStamp: data.created_at,
+          bio: data.bio,
+          repos: data.public_repos,
+          followers: data.followers,
+          following: data.following,
+          location: data.location,
+        }));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  };
+  useEffect(() => {
+    useFetchData('Yoana7183');
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {Object.keys(user).map((key) => (
+          <li key={key}>
+            {key}: {user[key]}
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GitHubFindDev;
