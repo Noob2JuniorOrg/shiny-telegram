b95600647d449cfe821a85e9659c595f2d831c63
y nakova <yoana.nakova@yahoo.com>
2023-05-23 11:07:40 +0300

    added new custom hook for fetching data from api

---
+++ hooks/useFetchData.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+function useFetchData(userName) {
+  const [fetchedData, setfetchedData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://api.github.com/users/${userName}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_KEY}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          setError(true);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setfetchedData(data);
+        setIsLoading(false);
+        setError(false);
+      })
+      .catch((error) => {
+        setError(true);
+        console.log(error);
+      });
+  }, []);
+  if (fetchedData != null && !isLoading && error == false) {
+    console.log(fetchedData);
+    return fetchedData;
+  }
+  if (isLoading == true) {
+    return isLoading;
+  }
+
+  if (fetchedData == null && !isLoading && error == true) {
+    return error;
+  }
+}
+
+export default useFetchData;
