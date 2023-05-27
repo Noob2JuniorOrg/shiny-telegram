e42d5839cfe44998f9c038fefbc32271b67bad00
y nakova <yoana.nakova@yahoo.com>
2023-05-27 22:48:28 +0300

    fixed cutsom hook/ added context to provide children with data object/ all components are full with information/ no UI/ functionality full

--- components/searchBar.jsx
+++ components/searchBar.jsx
@@ -1,8 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
+import { UserDataContext as UserError } from '../GitHubFindDev';
 const SearchBarAndSubmitButton = ({ getValue }) => {
   const inputRef = useRef(null);
+  const userData = useContext(UserError);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  useEffect(() => {
+    if (userData.error) {
+      setErrorMessage('No results');
+      console.log(errorMessage);
+    }
+    if (!userData.error) {
+      setErrorMessage('');
+    }
+  }, [userData.error]);
 
   const handleSubmit = () => {
     let value = inputRef.current.value.trim();
@@ -10,9 +22,20 @@ const SearchBarAndSubmitButton = ({ getValue }) => {
   };
 
   return (
-    <div>
-      <input type="text" ref={inputRef} />
-      <button onClick={handleSubmit}>Submit</button>
+    <div className="flex">
+      <div>{errorMessage}</div>
+      <input
+        className=" w-[625px] h-[69px]"
+        type="text"
+        ref={inputRef}
+        placeholder="Search GitHub username… "
+      />
+      <button
+        className="w-[106px] h-[50px] bg-buttonOnGitHubFindDevSearchButton"
+        onClick={handleSubmit}
+      >
+        Search
+      </button>
     </div>
   );
 };
