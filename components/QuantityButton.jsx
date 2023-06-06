0f6452304126593a542efd0284bc455ade53aac7
y nakova <yoana.nakova@yahoo.com>
2023-06-06 17:49:29 +0300

    added cart / quantity functionality/ total amount/ delete cart content

--- components/QuantityButton.jsx
+++ components/QuantityButton.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { QuantityContext } from '../EcommerceShop';
 
 const QuantityButton = () => {
+  const { quantity, setQuantity } = useContext(QuantityContext);
+  const [inputQuantity, setInptQuantity] = useState(0);
+  const handleMinusClick = () => {
+    if (quantity > 0) {
+      setInptQuantity((prevCount) => prevCount - 1);
+    }
+  };
+  const handlePlusClick = () => {
+    setInptQuantity((prevCount) => prevCount + 1);
+  };
+
+  const handleInputChange = (event) => {
+    const newQuantity = parseInt(event.target.value);
+    setInptQuantity(newQuantity);
+  };
   return (
     <div>
       <div className=" flex justify-between mt-10">
-        <div className="w-[157px] h-[56px]  bg-[#ececec] flex p-5 justify-center rounded-xl">
-          <div className="w-[15px]">
+        <div className="w-[157px] h-[56px]  bg-[#ececec] flex p-5 justify-between rounded-xl">
+          <div className="w-[15px] cursor-pointer " onClick={handleMinusClick}>
             <img
               className="bg-cover pt-1"
               src="src\feature-eCommerce\images\icon-minus.svg"
@@ -13,10 +29,15 @@ const QuantityButton = () => {
               srcSet=""
             />
           </div>
-          <input type="text" className="w-[80px] bg-[#ececec]" />
-          <div className="w-[15px]">
+          <input
+            type="text"
+            className=" w-[20px] bg-[#ececec]"
+            value={inputQuantity}
+            onChange={handleInputChange}
+          />
+          <div className="w-[15px] cursor-pointer" onClick={handlePlusClick}>
             <img
-              className="bg-cover"
+              className="bg-cover "
               src="src\feature-eCommerce\images\icon-plus.svg"
               alt=""
               srcSet=""
@@ -24,7 +45,12 @@ const QuantityButton = () => {
           </div>
         </div>
         <div className="w-[272px] h-[56px] bg-ecommerceOrangeColor hover:bg-hoverEcommerceOrangeColor flex justify-center rounded-xl text-white cursor-pointer">
-          <button className=" flex justify-center pt-4">
+          <button
+            className=" flex justify-center pt-4"
+            onClick={() => {
+              setQuantity(inputQuantity);
+            }}
+          >
             <div>
               <img
                 src="src\feature-eCommerce\images\icon-cart.svg"
