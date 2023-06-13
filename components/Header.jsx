4d328b6bd68b38c701fc46a6cd35f6fab9dddd4f
y nakova <yoana.nakova@yahoo.com>
2023-06-13 17:37:25 +0300

    added mobile modal menu with opacity/ need to ad </> buttons on main photo

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,57 +4,115 @@ import { InitialContext } from '../EcommerceShop';
 const Header = () => {
   const { initialStateObject, setinitialStateObject } =
     useContext(InitialContext);
-  const [isOpen, setIsOpen] = useState(true);
+  const [isCartHasBeenOpen, setIsCartHasBeenOpen] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const LinksStyle = ` cursor-pointer lg:text-base px-3.5 border-b-4 border-transparent  rounded-sm pb-10 hover:border-ecommerceOrangeColor md:text-sm`;
-
+  const LinksStyle = `cursor-pointer lg:text-base md:px-3.5 border-b-4 border-transparent rounded-sm pb-10 hover:border-ecommerceOrangeColor pt-[15rem] md:text-sm sm:text-xs sm:px-2`;
+  const eachLinkStyle = `cursor-pointer lg:text-base md:px-3.5 border-b-4 border-transparent rounded-sm pb-10 hover:border-ecommerceOrangeColor md:text-sm sm:text-xs sm:px-2`;
   const handleClick = () => {
-    setIsOpen(!isOpen);
+    setIsMenuOpen(false);
+    setIsCartHasBeenOpen(!isCartHasBeenOpen);
     setinitialStateObject((prev) => ({
       ...prev,
-      isCartOpen: isOpen,
+      isCartOpen: isCartHasBeenOpen,
     }));
   };
 
-  return (
-    <div className="flex justify-between border-b-[1px] border-[#E4E9F2]  ">
-      <div className="flex">
-        <div className="w-[200px] pr-20">
-          <img src="src\feature-eCommerce\images\logo.svg" alt="" srcSet="" />
-        </div>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-        <div className={LinksStyle}>Collections</div>
-        <div className={LinksStyle}>Men</div>
-        <div className={LinksStyle}>Woman</div>
-        <div className={LinksStyle}>About</div>
-        <div className={LinksStyle}>Contact</div>
+  return (
+    <div className="flex justify-between border-b-[1px] border-[#E4E9F2]">
+      <div className="block lg:hidden pt-7">
+        <button
+          onClick={toggleMenu}
+          className="sm:hidden flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+        >
+          {!isMenuOpen && (
+            <img
+              className="fill-current mt-[-2.6rem]"
+              src="/src/feature-eCommerce/images/icon-menu.svg"
+              alt=""
+            />
+          )}
+          {isMenuOpen && (
+            <img
+              className="fill-current mt-[-15rem] pb-28"
+              src="/src/feature-eCommerce/images/icon-close.png"
+              alt=""
+            />
+          )}
+        </button>
+      </div>
+      <div className="flex justify-start">
+        <div className="sm:w-[200px] sm:h-[20px] sm:pr-2 sm:mr-[-1.5rem] h-[25px] md:mr-20 mr-[2rem] w-[150px] md:flex flex">
+          <img src="/src/feature-eCommerce/images/logo.svg" alt="" />
+        </div>
+        {isMenuOpen && (
+          <div className="fixed top-0 left-0 w-screen h-screen bg-gray-800 bg-opacity-40 z-10 flex justify-start items-center">
+            <div className="w-[50%] h-full bg-white pt-20">
+              <button
+                onClick={toggleMenu}
+                className="absolute top-[7rem] left-[-20px] m-4 p-2 rounded-full text-black bg-white hover:bg-gray-200"
+              >
+                {isMenuOpen ? (
+                  <img
+                    className="w-8 h-8 fill-current"
+                    src="/src/feature-eCommerce/images/icon-close.png"
+                    alt=""
+                  />
+                ) : (
+                  <img
+                    className="w-4 h-4 fill-current"
+                    src="/src/feature-eCommerce/images/icon-menu.svg"
+                    alt=""
+                  />
+                )}
+              </button>
+              <div className={`sm:flex sm:justify-between ${LinksStyle}`}>
+                <div>Collections</div>
+                <div>Men</div>
+                <div>Woman</div>
+                <div>About</div>
+                <div>Contact</div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={isMenuOpen ? `hidden` : ` sm:flex sm::flex-row  hidden`}>
+        <div className={eachLinkStyle}>Collections</div>
+        <div className={eachLinkStyle}>Men</div>
+        <div className={eachLinkStyle}>Woman</div>
+        <div className={eachLinkStyle}>About</div>
+        <div className={eachLinkStyle}>Contact</div>
       </div>
       <div className="flex justify-center">
         <div
-          className=" cursor-pointer w-[22px] pt-5"
+          className="cursor-pointer w-[22px] pt-5"
           id="cart"
           onClick={handleClick}
         >
           {initialStateObject.quantity > 0 && (
-            <div className="w-[20px] h-[15px] bg-ecommerceOrangeColor text-white text-xs flex justify-center font-black rounded-xl">
+            <div
+              className={
+                isMenuOpen
+                  ? 'w-[20px] h-[15px] bg-ecommerceOrangeColor text-white text-xs flex justify-center font-black rounded-xl'
+                  : 'w-[20px] h-[15px] bg-ecommerceOrangeColor text-white text-xs flex justify-end font-black rounded-xl'
+              }
+            >
               {initialStateObject.quantity}
             </div>
           )}
-
           <img
             className="lg:mt-[-0.2rem] ml-[-0.5rem] md:mt-[-1.2rem]"
-            src="src\feature-eCommerce\images\icon-cart.svg"
+            src="/src/feature-eCommerce/images/icon-cart.svg"
             alt=""
-            srcSet=""
           />
         </div>
         <div className="pl-5 w-[70px] cursor-pointer mt-[-0.2rem] ">
-          <img
-            className=""
-            src="src\feature-eCommerce\images\image-avatar.png"
-            alt=""
-            srcSet=""
-          />
+          <img src="/src/feature-eCommerce/images/image-avatar.png" alt="" />
         </div>
       </div>
     </div>
