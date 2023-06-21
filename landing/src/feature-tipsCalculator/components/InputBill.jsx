c1fb4bf00e1acaa78ad47da297c34d0f748bcf1a
y nakova <yoana.nakova@yahoo.com>
2023-06-21 16:06:37 +0300

    added custom hook for validation / direct update context object if is valid /

--- landing/src/feature-tipsCalculator/components/InputBill.jsx
+++ landing/src/feature-tipsCalculator/components/InputBill.jsx
@@ -1,45 +1,17 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
-import { TipsContext as BillConext } from '../Tips_Calculator';
+import useValidateNumberInput from '../hooks/useValidateNumberInput';
 
 const InputBill = () => {
-  const { inputsInObject, setinputsInObject } = useContext(BillConext);
   const [error, setError] = useState('');
-  const [bill, setBill] = useState(0);
-  //checking for the value in the context, on reset the input value is set to 0
-  useEffect(() => {
-    if (inputsInObject.bill == 0) {
-      inputsInObject.bill = '';
-      setError('');
-    }
-  }, [inputsInObject]);
-
+  const [bill, setBill] = useState('');
   const handleChange = (event) => {
-    let billAmount = event.target.value.trim();
-    setBill(billAmount);
+    setBill(event.target.value.trim());
   };
+  // custom hook for validation on input, and if is valid will update the context
+  const validInput = useValidateNumberInput(setError, bill, setBill, ['bill']);
+  const value = bill === 0 ? '' : validInput;
 
-  useEffect(() => {
-    if (/^[a-zA-Z]+$/g.test(bill)) {
-      setError(`Please enter a number`);
-    }
-    if (isNaN(bill)) {
-      setError(`Please enter a number`);
-    } else if (bill < 0) {
-      setError(`Can't be negative`);
-    } else if (bill === undefined) {
-      setError('');
-    } else {
-      setinputsInObject((prev) => ({ ...prev, bill: bill }));
-      setError('');
-    }
-  }, [bill]);
-  //clearing 0 as the value in the context so that it does not appear in the input when the calculation is reset
-  if (inputsInObject.bill === 0) {
-    inputsInObject.bill = '';
-  }
-
-  let value = bill == 0 ? '' : inputsInObject.bill;
   return (
     <div className="pb-10">
       <div className="flex justify-between">
@@ -55,7 +27,6 @@ const InputBill = () => {
           alt="icon-dollar"
           srcSet=""
         />
-
         <input
           inputMode="numeric"
           className=" bg-teal-50 rounded-md h-[48px] w-[100%] lg:h-[48px] text-2xl text-right pr-4 sm:h-[70px]"
